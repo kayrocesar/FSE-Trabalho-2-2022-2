c67e0d12f89b37b939ffff81cff89913520b4105
kayrocesar <kayro.cesar.kc@gmail.com>
2023-01-23 20:53:00 -0300

    amb_sensor

--- src/sensorBME280.py
+++ src/sensorBME280.py
@@ -1,23 +1,10 @@
 import smbus2
 import bme280
 
-
-data=None
-bus= None
-address = 0x76
-calibration_params=None
-
 def init_i2c():
-        global data, bus, calibration_params,address
-        port = 1
-        address = 0x76
-        bus = smbus2.SMBus(port)
-        calibration_params = bme280.load_calibration_params(bus, address)
-        data = bme280.sample(bus, address, calibration_params)
-        
-
-def readAmbientTemperature():
-        global data, bus, calibration_params,address
-        data = bme280.sample(bus, address, calibration_params)
-        print("Temperatura ambiente: ",data.temperature)
-        return data.temperature
+  port = 1
+  address = 0x76
+  bus = smbus2.SMBus(port)
+  calibration_params = bme280.load_calibration_params(bus, address)
+  data = bme280.sample(bus, address, calibration_params)
+  return data.temperature
